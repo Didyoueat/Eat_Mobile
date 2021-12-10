97912d29ef8bd2dcd3a95277411ada163071e6f5
KOREAparksh <tmdgks2222@naver.com>
2021-12-10 15:11:57 +0900

    DYE-34 ➕ ReBuild: (버전문제로 인한 재빌드) models클래스 구현 및 json_serializable를 이용한 g파일 생성

---
+++ dye/lib/models/shop.g.dart
@@ -0,0 +1,58 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'shop.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Shop _$ShopFromJson(Map<String, dynamic> json) => Shop(
+      json['shopId'] as String?,
+      json['businessNumber'] as String?,
+      json['businessName'] as String?,
+      json['businessPhone'] as String?,
+      json['dayoff'] as int?,
+      json['address'] as String?,
+      json['latitude'] as String?,
+      json['longitude'] as String?,
+      json['name'] as String?,
+      json['phone'] as String?,
+      json['origin'] as String?,
+      json['content'] as String?,
+      (json['imageUrl'] as List<dynamic>?)?.map((e) => e as String?).toList(),
+      json['officeHourStart'] as String?,
+      json['officeHourEnd'] as String?,
+      json['temporaryDayStart'] as String?,
+      json['temporaryDayEnd'] as String?,
+      json['createdAt'] as String?,
+      json['updatedAt'] as String?,
+      (json['dishes'] as List<dynamic>?)
+          ?.map((e) =>
+              e == null ? null : Dish.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      json['distance'] as String?,
+    );
+
+Map<String, dynamic> _$ShopToJson(Shop instance) => <String, dynamic>{
+      'shopId': instance.shopId,
+      'businessNumber': instance.businessNumber,
+      'businessName': instance.businessName,
+      'businessPhone': instance.businessPhone,
+      'dayoff': instance.dayoff,
+      'address': instance.address,
+      'latitude': instance.latitude,
+      'longitude': instance.longitude,
+      'name': instance.name,
+      'phone': instance.phone,
+      'origin': instance.origin,
+      'content': instance.content,
+      'imageUrl': instance.imageUrl,
+      'officeHourStart': instance.officeHourStart,
+      'officeHourEnd': instance.officeHourEnd,
+      'temporaryDayStart': instance.temporaryDayStart,
+      'temporaryDayEnd': instance.temporaryDayEnd,
+      'createdAt': instance.createdAt,
+      'updatedAt': instance.updatedAt,
+      'dishes': instance.dishes,
+      'distance': instance.distance,
+    };
