1d7ca24b09a726a7bc4aeda9de8d21e6c0cc7416
KOREAparksh <tmdgks2222@naver.com>
2022-01-18 21:26:33 +0900

    DYE-181 ✨ Feat: CustomInfoAppbar 변경된 기능 구현

--- dye/lib/widgets/custom_info_appbar.dart
+++ dye/lib/widgets/custom_info_appbar.dart
@@ -1,4 +1,6 @@
 import 'package:dye/constants/colors.dart';
+import 'package:dye/main.dart';
+import 'package:dye/widgets/custom_visibility.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -11,18 +13,26 @@ class CustomInfoAppBar extends StatelessWidget with PreferredSizeWidget {
     Key? key,
     required this.daysOption,
     required this.nowWeek,
-  })  : preferredSize = Size.fromHeight(72.h),
+  })  : preferredSize = Size.fromHeight(60.h),
         super(key: key);
 
   @override
   final Size preferredSize;
   final List<bool> daysOption;
   final int nowWeek;
+
   final List<String> _weekName = const ["월", "화", "수", "목", "금", "토", "일"];
-  final circleWidth = 36.w;
-  final circleHeight = 36.h;
-  final padding = 12.w;
-  final List<Widget> list = [];
+  final _weekButtonWidth = 36.w;
+  final _weekButtonHeight = 44.h;
+  final _circleHeight = 36.h;
+  final _circleTextSize = 1.3;
+  final _buttonMargin = 12.w;
+  final _cartInfoContainerHeight = 17.h;
+  final _cartInfoContainerRadius = 5.0;
+  final _cartInfoTextSize = 12.sp;
+  final _cartInfoTextHeight = 1.26;
+  final _appBarBottomPadding = EdgeInsets.only(bottom: 8.h);
+  final List<Widget> _weekButtonList = [];
 
   @override
   Widget build(BuildContext context) {
@@ -39,45 +49,81 @@ class CustomInfoAppBar extends StatelessWidget with PreferredSizeWidget {
   }
 
   Widget _weekButtons() {
-    for (int i = 0; i < 7; i++) {
-      if (daysOption[i] == true) {
-        list.add(
-          Column(
-            children: [
-              CircleButton(
-                width: circleWidth,
-                height: circleHeight,
-                onTap: () {},
-                child: Text(
-                  _weekName[i],
-                  style: TextStyle(color: textColorGray),
-                ),
-              ),
-              Visibility(
-                visible: (i == nowWeek) ? true : false,
-                maintainState: true,
-                maintainSize: true,
-                maintainAnimation: true,
-                child: SvgPicture.asset(
-                  "assets/icons/cart.svg",
-                  width: 15.h,
-                  color: mainColor,
-                ),
-              ),
-            ],
-          ),
-        );
-      }
-      if (daysOption[i] == true) list.add(SizedBox(width: padding));
-    }
-    list.removeAt(list.length - 1);
-
+    _setWeekButtons();
     return Container(
-      padding: EdgeInsets.only(bottom: 10.h),
+      padding: _appBarBottomPadding,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
-        // crossAxisAlignment: CrossAxisAlignment.end,
-        children: list,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: _weekButtonList,
+      ),
+    );
+  }
+
+  void _setWeekButtons() {
+    for (int i = 0; i < 7; i++) {
+      if (daysOption[i] == true) {
+        _weekButtonList.add(_weekButton(i));
+        _weekButtonList.add(SizedBox(width: _buttonMargin));
+      }
+    }
+    _weekButtonList.removeAt(_weekButtonList.length - 1);
+  }
+
+  Widget _weekButton(int i) {
+    return SizedBox(
+      width: _weekButtonWidth,
+      height: _weekButtonHeight,
+      child: Stack(
+        children: [
+          _circleButton(i),
+          _cartInfoContainer(i),
+        ],
+      ),
+    );
+  }
+
+  Widget _circleButton(int i) {
+    return Container(
+      alignment: Alignment.topCenter,
+      child: CircleButton(
+        height: _circleHeight,
+        onTap: () {},
+        borderColor: (i == nowWeek) ? mainColor : null,
+        child: Text(
+          _weekName[i],
+          style: TextStyle(
+            color: (i == nowWeek) ? mainColor : borderColorAA,
+            height: _circleTextSize,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _cartInfoContainer(int i) {
+    return Container(
+      alignment: Alignment.bottomCenter,
+      child: CustomVisibility(
+        visible:
+            true, //(i == nowWeek) ? true : false, //TODO 선택로직이 아니라 장바구니 개수에 따라 되게끔
+        child: Container(
+          height: _cartInfoContainerHeight,
+          decoration: BoxDecoration(
+            color: (i == nowWeek) ? mainColor : borderColorAA,
+            borderRadius: BorderRadius.circular(_cartInfoContainerRadius),
+          ),
+          child: Center(
+            child: Text(
+              "1",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: _cartInfoTextSize,
+                height: _cartInfoTextHeight,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
